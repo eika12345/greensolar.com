3088b837fe32e19ad20f9bd02e78a5700c7de00b
Eika <3r1kas123@gmail.com>
2026-06-10 18:49:38 +0200

    biiig footer nav update

--- assets/js/main.js
+++ assets/js/main.js
@@ -137,4 +137,15 @@
 
 			});
 
+			// Smooth scroll to anchor links
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 800);
+    }
+});
+
 })(jQuery);
